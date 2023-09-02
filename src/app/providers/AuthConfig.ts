a5fd649f216af1af3afc9611df79b4e3d0120794
Ramzi <ramzi.abu.qassim@nav.no>
2023-09-02 19:08:40 +0200

    bruk .env for authConfig

--- src/app/providers/AuthConfig.ts
+++ src/app/providers/AuthConfig.ts
@@ -2,9 +2,9 @@ import * as msal from '@azure/msal-node';
 
 const authConfig: msal.Configuration = {
 	auth: {
-		clientId: 'xxx',
-		authority: `xxx`,
-		clientSecret: 'xxx',
+		clientId: process.env.AZURE_AD_CLIENT_ID!,
+		authority: `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}`,
+		clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
 	},
 };
 
